c3008f2f0586dba3836732e52270c32767ffbe52
Paulino Gomes <paulinosgomes@gmail.com>
2021-12-28 19:07:37 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'sintonia_player',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
